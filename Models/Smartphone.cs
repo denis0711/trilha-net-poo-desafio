9e683f41515fd3d98a560d252535f7fe11609a56
denis0711 <denisapsilva07@gmail.com>
2023-12-05 19:33:18 -0300

    Atualizando o Projeto da DIO coloquei tambem o ANo da fabricação e Retornar a informações do Smartphone antes de rodar os metodos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,40 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models;
+
+public abstract class Smartphone
 {
-    public abstract class Smartphone
+    public string Numero { get; set; }
+    public string Modelo { get; set; }
+    public string IMEI { get; set; }
+    public int Memoria { get; set; }
+    public DateTime DataFabricacao { get; set; }
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+
+    protected Smartphone(string numero, string modelo, string iMEI, int memoria, DateTime dataFabricacao)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        Numero = numero;
+        Modelo = modelo;
+        IMEI = iMEI;
+        Memoria = memoria;
+        DataFabricacao = dataFabricacao;
     }
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public void RetornarInformacaoSmartphone()
+    {
+        Console.WriteLine($"Numero: {this.Numero}\nModelo: {this.Modelo}\n" +
+                          $"IMEI: {this.IMEI}\nMemoria: {this.Memoria}\n" +
+                          $"Ano da Fabricacao: {this.DataFabricacao.ToString("yyyy")}");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+
+Nokia nokia = new Nokia("97757-0831", "N9", "312312312", 5, DateTime.Now.AddYears(-10));
+nokia.RetornarInformacaoSmartphone();
+Console.WriteLine();
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n--------------------------------------------------------------------\n");
+
+Iphone iphone = new Iphone("97757-0122", "Iphone X", "3112212312", 10, DateTime.Now.AddYears(-3));
+iphone.RetornarInformacaoSmartphone();
+Console.WriteLine();
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
